f67405d4699a13dec735febeebb83a1cbf05d134
Kevin Gao <k_gao@aol.com>
2020-08-11 15:03:27 +0200

    uploadImg unit test

---
+++ server/src/utils/__tests__/uploadImg.test.js
@@ -0,0 +1,13 @@
+const cloudinary = require('cloudinary');
+const uploadImg = require('../uploadImg');
+const {
+  mockUploadResponse,
+} = require('../../__tests__/__mocks__/createInitData');
+
+jest.mock('cloudinary');
+
+it('Should parse response object to string', async () => {
+  cloudinary.v2.uploader.upload.mockResolvedValue(mockUploadResponse);
+  const imgData = await uploadImg();
+  expect(imgData).toEqual(JSON.stringify(mockUploadResponse));
+});

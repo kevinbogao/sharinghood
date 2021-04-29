a2b19846d79ce904dc68afd5b16fb438f6f88472
Kevin Gao <k_gao@aol.com>
2021-04-29 19:04:33 +0200

    refactor(server): thread models to TS

---
+++ server/src/models/thread.ts
@@ -0,0 +1,27 @@
+import { Schema, Document, model } from "mongoose";
+
+interface IThread extends Document {
+  content: string;
+  poster: Schema.Types.ObjectId;
+  community: Schema.Types.ObjectId;
+}
+
+const threadSchema: Schema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    poster: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    community: {
+      type: Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model<IThread>("Thread", threadSchema);

4b4afaf8926549a20177b235b59df680a46d48b8
Kevin Gao <k_gao@aol.com>
2021-05-01 00:59:51 +0200

    refactor(server): bookings resolver to typeDefs

--- server/src/resolvers/bookings.ts
+++ server/src/resolvers/bookings.ts
@@ -1,17 +1,30 @@
-// @ts-nocheck
-import AuthenticationError from "apollo-server";
-import User from "../models/user";
-import Post from "../models/post";
-import Booking from "../models/booking";
-import Notification from "../models/notification";
+import { AuthenticationError } from "apollo-server";
+import User, { UserDocument } from "../models/user";
+import Post, { PostDocument } from "../models/post";
+import Booking, { BookingDocument } from "../models/booking";
+import Notification, { NotificationDocument } from "../models/notification";
+import { UserContext } from "../types";
 
 const updateBookingMail = require("../utils/sendMail/updateBookingMail");
 const pushNotification = require("../utils/pushNotification");
 
+export interface BookingInput {
+  postId: string;
+  bookingId: string;
+  communityId: string;
+  notificationId: string;
+  status: number;
+  dateType: number;
+  dateNeed: Date;
+  dateReturn: Date;
+  notifyContent: string;
+  notifyRecipientId: string;
+}
+
 const bookingsResolvers = {
   Mutation: {
     updateBooking: async (
-      _,
+      _: unknown,
       {
         bookingInput: {
           status,
@@ -21,67 +34,81 @@ const bookingsResolvers = {
           notifyContent,
           notifyRecipientId,
         },
-      },
-      { user, redis }
+      }: { bookingInput: BookingInput },
+      { user, redis }: { user: UserContext; redis: any }
     ) => {
       if (!user) throw new AuthenticationError("Not Authenticated");
 
       try {
         // Find booking, notification & recipient by id
-        const [booking, notification, recipient] = await Promise.all([
-          Booking.findById(bookingId).populate("post"),
-          Notification.findById(notificationId),
-          User.findById(notifyRecipientId).lean(),
-        ]);
+        const booking: BookingDocument | null = await Booking.findById(
+          bookingId
+        ).populate("post");
+        const notification: NotificationDocument | null = await Notification.findById(
+          notificationId
+        );
+        const recipient: UserDocument | null = await User.findById(
+          notifyRecipientId
+        ).lean();
 
-        // Get post & check if current user is post creator
-        // && throw error if not
-        const post = await Post.findById(booking.post).lean();
-        if (!post.creator.equals(user.userId)) {
-          throw new Error("Unauthorized user");
+        if (booking && notification && recipient) {
+          // Get post & check if current user is post creator
+          // && throw error if not
+
+          const post: PostDocument | null = await Post.findById(
+            booking.post
+          ).lean();
+
+          if (post) {
+            if (post.creator.toString() !== user.userId) {
+              throw new Error("Unauthorized user");
+            }
+
+            // Update booking status
+            booking.status = status;
+
+            // Set recipient's notification isRead status to false
+            notification.isRead[notifyRecipientId] = false;
+            notification.markModified("isRead");
+
+            // Save booking & send booking notification email if user is notified
+            await Promise.all([
+              booking.save(),
+              notification.save(),
+              process.env.NODE_ENV === "production" &&
+                recipient.isNotified &&
+                updateBookingMail(
+                  `${process.env.ORIGIN}/notifications`,
+                  recipient.email,
+                  notifyContent
+                ),
+              // Set communityId key to notifications:userId hash in redis
+              redis.hset(
+                `notifications:${notifyRecipientId}`,
+                `${communityId}`,
+                true
+              ),
+            ]);
+
+            // Send push notification to requester
+            pushNotification(
+              { communityId },
+              `${user.userName} ${
+                status === 1 ? "accepted" : "denied"
+              } your booking on ${post.title}`,
+              [
+                {
+                  _id: recipient._id,
+                  fcmTokens: recipient.fcmTokens,
+                },
+              ]
+            );
+
+            return booking;
+          }
         }
 
-        // Update booking status
-        booking.status = status;
-
-        // Set recipient's notification isRead status to false
-        notification.isRead[notifyRecipientId] = false;
-        notification.markModified("isRead");
-
-        // Save booking & send booking notification email if user is notified
-        await Promise.all([
-          booking.save(),
-          notification.save(),
-          process.env.NODE_ENV === "production" &&
-            recipient.isNotified &&
-            updateBookingMail(
-              `${process.env.ORIGIN}/notifications`,
-              recipient.email,
-              notifyContent
-            ),
-          // Set communityId key to notifications:userId hash in redis
-          redis.hset(
-            `notifications:${notifyRecipientId}`,
-            `${communityId}`,
-            true
-          ),
-        ]);
-
-        // Send push notification to requester
-        pushNotification(
-          { communityId },
-          `${user.userName} ${
-            status === 1 ? "accepted" : "denied"
-          } your booking on ${post.title}`,
-          [
-            {
-              _id: recipient._id,
-              fcmTokens: recipient.fcmTokens,
-            },
-          ]
-        );
-
-        return booking;
+        return null;
       } catch (err) {
         throw new Error(err);
       }

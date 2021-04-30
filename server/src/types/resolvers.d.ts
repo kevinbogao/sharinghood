54d0fb69b069f54bf812d6c64aa0be9a8316a371
Kevin Gao <k_gao@aol.com>
2021-05-01 01:02:30 +0200

    refactor(server): remove old js files

--- server/src/types/resolvers.d.ts
+++
@@ -1,9 +0,0 @@
-export interface PostInput {
-  postId?: mongoose.Schema.Types.ObjectId;
-  title: string;
-  desc: string;
-  image: any;
-  condition: number;
-  isGiveaway?: boolean;
-  requesterId?: string;
-}

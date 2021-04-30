e30d3f4670095eed0edd26d8435cfed9cf039e3c
Kevin Gao <k_gao@aol.com>
2021-05-01 01:02:58 +0200

    refactor(server): requests resolver to typescript

--- server/src/resolvers/requests.ts
+++ server/src/resolvers/requests.ts
@@ -1,24 +1,38 @@
-// @ts-nocheck
 import { AuthenticationError, ForbiddenError } from "apollo-server";
-import mongoose from "mongoose";
+import { Types } from "mongoose";
 import User from "../models/user";
 import Thread from "../models/thread";
-import Request from "../models/request";
-import Community from "../models/community";
+import Request, { RequestDocument } from "../models/request";
+import Community, { CommunityDocument } from "../models/community";
+import { UserContext } from "../types";
+
 const uploadImg = require("../utils/uploadImg");
 const newRequestMail = require("../utils/sendMail/newRequestMail");
 const pushNotification = require("../utils/pushNotification");
 
+interface RequestInput {
+  title: string;
+  desc: string;
+  image: string;
+  dateType: number;
+  dateNeed?: Date;
+  dateReturn?: Date;
+}
+
 const requestsResolvers = {
   Query: {
-    request: async (_, { requestId }, { user }) => {
+    request: async (
+      _: unknown,
+      { requestId }: { requestId: string },
+      { user }: { user: UserContext }
+    ): Promise<RequestDocument> => {
       if (!user) throw new AuthenticationError("Not Authenticated");
 
       try {
         // Get request && lookup creator & threads
-        const request = await Request.aggregate([
+        const request: Array<RequestDocument> = await Request.aggregate([
           {
-            $match: { _id: mongoose.Types.ObjectId(requestId) },
+            $match: { _id: Types.ObjectId(requestId) },
           },
           {
             $lookup: {
@@ -44,45 +58,51 @@ const requestsResolvers = {
         throw new Error(err);
       }
     },
-    requests: async (_, { communityId }, { user }) => {
+    requests: async (
+      _: unknown,
+      { communityId }: { communityId: string },
+      { user }: { user: UserContext }
+    ): Promise<Array<RequestDocument | Types.ObjectId>> => {
       if (!user) throw new AuthenticationError("Not Authenticated");
 
       try {
         // Get all requests from given community
-        const communityRequests = await Community.aggregate([
-          {
-            $match: { _id: mongoose.Types.ObjectId(communityId) },
-          },
-          {
-            $lookup: {
-              from: "requests",
-              let: { requests: "$requests" },
-              pipeline: [
-                { $match: { $expr: { $in: ["$_id", "$$requests"] } } },
-                {
-                  $lookup: {
-                    from: "users",
-                    let: { creator: "$creator" },
-                    pipeline: [
-                      { $match: { $expr: { $eq: ["$_id", "$$creator"] } } },
-                    ],
-                    as: "creator",
-                  },
-                },
-                { $unwind: "$creator" },
-                {
-                  $sort: {
-                    createdAt: -1,
-                  },
-                },
-              ],
-              as: "requests",
+        const communityRequests: Array<CommunityDocument> = await Community.aggregate(
+          [
+            {
+              $match: { _id: Types.ObjectId(communityId) },
             },
-          },
-          {
-            $project: { requests: 1 },
-          },
-        ]);
+            {
+              $lookup: {
+                from: "requests",
+                let: { requests: "$requests" },
+                pipeline: [
+                  { $match: { $expr: { $in: ["$_id", "$$requests"] } } },
+                  {
+                    $lookup: {
+                      from: "users",
+                      let: { creator: "$creator" },
+                      pipeline: [
+                        { $match: { $expr: { $eq: ["$_id", "$$creator"] } } },
+                      ],
+                      as: "creator",
+                    },
+                  },
+                  { $unwind: "$creator" },
+                  {
+                    $sort: {
+                      createdAt: -1,
+                    },
+                  },
+                ],
+                as: "requests",
+              },
+            },
+            {
+              $project: { requests: 1 },
+            },
+          ]
+        );
 
         return communityRequests[0].requests;
       } catch (err) {
@@ -92,15 +112,15 @@ const requestsResolvers = {
   },
   Mutation: {
     createRequest: async (
-      _,
+      _: unknown,
       {
         requestInput: { title, desc, image, dateType, dateNeed, dateReturn },
         communityId,
-      },
-      { user }
+      }: { requestInput: RequestInput; communityId: string },
+      { user }: { user: UserContext }
     ) => {
       if (!user) throw new AuthenticationError("Not Authenticated");
-      const { userId, userName } = user;
+      const { userId, userName }: { userId: string; userName: string } = user;
 
       try {
         // Upload image to Cloudinary
@@ -126,59 +146,72 @@ const requestsResolvers = {
           }),
         ]);
 
-        // Save requestId & notificationId to community && requestId to creator
-        community.requests.push(request);
-        creator.requests.push(request);
+        if (creator && community) {
+          // Save requestId & notificationId to community && requestId to creator
+          community.requests.push(request);
+          creator.requests.push(request);
 
-        // Parse array of members object into array of emails if member is notified
-        const emails = community.members
-          .filter((member) => member.isNotified === true)
-          .map((member) => member.email);
+          // Parse array of members object into array of emails if member is notified
+          const emails = community.members
+            // @ts-ignore
+            .filter((member) => member.isNotified === true)
+            // @ts-ignore
+            .map((member) => member.email);
 
-        // Save community & sent email to subscribed users
-        await Promise.all([
-          community.save(),
-          creator.save(),
-          process.env.NODE_ENV === "production" &&
-            emails.length &&
-            newRequestMail(
-              userName,
-              title,
-              JSON.parse(imgData).secure_url,
-              `${process.env.ORIGIN}/requests/${request._id}`,
-              dateNeed,
-              emails,
-              `${userName} requested ${title} in your community.`
-            ),
-        ]);
+          // Save community & sent email to subscribed users
+          await Promise.all([
+            community.save(),
+            creator.save(),
+            process.env.NODE_ENV === "production" &&
+              emails.length &&
+              newRequestMail(
+                userName,
+                title,
+                JSON.parse(imgData).secure_url,
+                `${process.env.ORIGIN}/requests/${request._id}`,
+                dateNeed,
+                emails,
+                `${userName} requested ${title} in your community.`
+              ),
+          ]);
 
-        // Get a list of users that has FCM tokens
-        const receivers = community.members
-          .filter((member) => member.fcmTokens.length)
-          .map((member) => ({
-            _id: member._id,
-            fcmTokens: member.fcmTokens,
-          }));
+          // Get a list of users that has FCM tokens
+          const receivers = community.members
+            // @ts-ignore
+            .filter((member) => member.fcmTokens.length)
+            .map((member) => ({
+              // @ts-ignore
+              _id: member._id,
+              // @ts-ignore
+              fcmTokens: member.fcmTokens,
+            }));
 
-        // Sent push notification
-        pushNotification(
-          {},
-          `${userName} requested ${title} in the ${community.name} community`,
-          receivers
-        );
+          // Sent push notification
+          pushNotification(
+            {},
+            `${userName} requested ${title} in the ${community.name} community`,
+            receivers
+          );
 
-        return {
-          ...request._doc,
-          creator: {
-            _id: userId,
-            name: userName,
-          },
-        };
+          return {
+            ...request,
+            creator: {
+              _id: userId,
+              name: userName,
+            },
+          };
+        }
+
+        return null;
       } catch (err) {
         throw new Error(err);
       }
     },
-    deleteRequest: async (_, { requestId }, { user }) => {
+    deleteRequest: async (
+      _: unknown,
+      { requestId }: { requestId: string },
+      { user }: { user: UserContext }
+    ): Promise<RequestDocument | null> => {
       if (!user) throw new AuthenticationError("Not Authenticated");
 
       try {
@@ -188,31 +221,35 @@ const requestsResolvers = {
           User.findById(user.userId),
         ]);
 
-        // Throw error if user is not post creator
-        if (!request.creator.equals(user.userId)) {
-          throw new ForbiddenError("Unauthorized user");
+        if (request && currentUser) {
+          // Throw error if user is not post creator
+          if (request.creator.toString() !== user.userId) {
+            throw new ForbiddenError("Unauthorized user");
+          }
+
+          // Destruct threads from request
+          const { threads } = request;
+
+          // Delete request, requestId from community & delete request threads
+          await Promise.all([
+            request.remove(),
+            User.updateOne(
+              { _id: user.userId },
+              { $pull: { requests: requestId } }
+            ),
+            Community.updateMany(
+              { _id: { $in: currentUser.communities } },
+              {
+                $pull: { requests: requestId },
+              }
+            ),
+            Thread.deleteMany({ _id: { $in: threads } }),
+          ]);
+
+          return request;
         }
 
-        // Destruct threads from request
-        const { threads } = request;
-
-        // Delete request, requestId from community & delete request threads
-        await Promise.all([
-          request.remove(),
-          User.updateOne(
-            { _id: user.userId },
-            { $pull: { requests: requestId } }
-          ),
-          Community.updateMany(
-            { _id: { $in: currentUser.communities } },
-            {
-              $pull: { requests: requestId },
-            }
-          ),
-          Thread.deleteMany({ _id: { $in: threads } }),
-        ]);
-
-        return request;
+        return null;
       } catch (err) {
         throw new Error(err);
       }

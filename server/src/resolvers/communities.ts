ef9f357340fdf93d98da45eee43d0ce50a96dccb
Kevin Gao <k_gao@aol.com>
2021-05-01 00:56:47 +0200

    refactor(server): communities resolvers to typescript

--- server/src/resolvers/communities.ts
+++ server/src/resolvers/communities.ts
@@ -1,26 +1,30 @@
-// @ts-nocheck
 import { AuthenticationError } from "apollo-server";
-import { mongoose } from "mongoose";
-const User = require("../models/user");
-import Community from "../models/community";
+import { Types } from "mongoose";
+import User, { UserDocument } from "../models/user";
+import Community, { CommunityDocument } from "../models/community";
+import { UserContext } from "../types";
 const handleErrors = require("../utils/handleErrors");
 
-import { ICommunity } from "../types/models";
-import { UserContext } from "../types/server";
+export interface CommunityInput {
+  name: string;
+  code: string;
+  zipCode?: string;
+  password?: string;
+}
 
 const communitiesResolvers = {
   Query: {
     community: async (
-      _: any,
+      _: unknown,
       {
         communityId,
         communityCode,
       }: { communityId: string; communityCode: string }
-    ) => {
+    ): Promise<CommunityDocument> => {
       try {
         // Find community by community code if communityCode is given
         // else find community by id & populate users
-        const community: Array<ICommunity> = await Community.aggregate([
+        const community: Array<CommunityDocument> = await Community.aggregate([
           {
             $match: {
               ...(communityCode
@@ -28,7 +32,7 @@ const communitiesResolvers = {
                   { code: communityCode }
                 : // Query by community id
                   {
-                    _id: mongoose.Types.ObjectId(communityId),
+                    _id: Types.ObjectId(communityId),
                   }),
             },
           },
@@ -48,15 +52,15 @@ const communitiesResolvers = {
       }
     },
     communities: async (
-      _: any,
-      __: any,
+      _: unknown,
+      __: unknown,
       { user, redis }: { user: UserContext; redis: any }
-    ) => {
+    ): Promise<Array<CommunityDocument> | number> => {
       if (!user) throw new AuthenticationError("Not Authenticated");
 
       try {
-        const userCommunities: Array<IUser> = await User.aggregate([
-          { $match: { _id: mongoose.Types.ObjectId(user.userId) } },
+        const userCommunities: Array<UserDocument> = await User.aggregate([
+          { $match: { _id: Types.ObjectId(user.userId) } },
           {
             $lookup: {
               from: "communities",
@@ -67,22 +71,26 @@ const communitiesResolvers = {
           },
         ]);
 
-        // TODO: remove Promise.all()
-        // Get hasNotifications value from redis hash for each community of user
-        const communities = await Promise.all(
-          userCommunities[0].communities.map(async (community) => {
-            const hasNotifications = await redis.hget(
-              `notifications:${user.userId}`,
-              `${community._id}`
-            );
-            return {
-              ...community,
-              hasNotifications: hasNotifications === "true" || false,
-            };
-          })
-        );
+        if (userCommunities.length > 0) {
+          // TODO: remove Promise.all()
+          // Get hasNotifications value from redis hash for each community of user
+          const communities = await Promise.all(
+            userCommunities[0].communities.map(async (community: any) => {
+              const hasNotifications = await redis.hget(
+                `notifications:${user.userId}`,
+                `${community._id}`
+              );
+              return {
+                ...community,
+                hasNotifications: hasNotifications === "true" || false,
+              };
+            })
+          );
 
-        return communities;
+          return communities;
+        }
+
+        return 0;
       } catch (err) {
         throw new Error(err);
       }
@@ -90,52 +98,68 @@ const communitiesResolvers = {
   },
   Mutation: {
     createCommunity: async (
-      _,
-      { communityInput: { name, code, zipCode } },
-      { user }
-    ) => {
+      _: unknown,
+      {
+        communityInput: { name, code, zipCode },
+      }: { communityInput: CommunityInput },
+      { user }: { user: UserContext }
+    ): Promise<CommunityDocument | null> => {
       if (!user) throw new AuthenticationError("Not Authenticated");
 
       try {
         // Create & save community && get user
-        const [community, currentUser] = await Promise.all([
-          Community.create({
-            name,
-            code,
-            zipCode,
-            creator: user.userId,
-          }),
-          User.findById(user.userId),
-        ]);
+        const community: CommunityDocument = await Community.create({
+          name,
+          code,
+          zipCode,
+          creator: user.userId,
+        });
+        const currentUser: UserDocument | null = await User.findById(
+          user.userId
+        );
 
-        // Add user to community, and add community to user
-        community.members.push(user.userId);
-        currentUser.communities.push(community._id);
-        await Promise.all([community.save(), currentUser.save()]);
+        if (community && currentUser) {
+          // Add user to community, and add community to user
+          community.members.push(Types.ObjectId(user.userId));
+          currentUser.communities.push(community._id);
+          await Promise.all([community.save(), currentUser.save()]);
 
-        return community;
+          return community;
+        }
+
+        return null;
       } catch (err) {
         // Throw duplicate error
         return handleErrors(err);
       }
     },
-    joinCommunity: async (_, { communityId }, { user }) => {
+    joinCommunity: async (
+      _: unknown,
+      { communityId }: { communityId: string },
+      { user }: { user: UserContext }
+    ) => {
       if (!user) throw new AuthenticationError("Not Authenticated");
 
       try {
         // Get current user & community
-        const [currentUser, community] = await Promise.all([
-          User.findById(user.userId),
-          Community.findById(communityId),
-        ]);
+        const community: CommunityDocument | null = await Community.findById(
+          communityId
+        );
+        const currentUser: UserDocument | null = await User.findById(
+          user.userId
+        );
 
-        // Save user to community members and save community to
-        // user communities
-        currentUser.communities.push(communityId);
-        community.members.push(user.userId);
-        await Promise.all([currentUser.save(), community.save()]);
+        if (community && currentUser) {
+          // Save user to community members and save community to
+          // user communities
+          currentUser.communities.push(Types.ObjectId(communityId));
+          community.members.push(Types.ObjectId(user.userId));
+          await Promise.all([currentUser.save(), community.save()]);
 
-        return community;
+          return community;
+        }
+
+        return null;
       } catch (err) {
         throw new Error(err);
       }

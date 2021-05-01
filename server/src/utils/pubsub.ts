3bbdbbf792a1230098a210e35c5bdd355cec7bd4
Kevin Gao <k_gao@aol.com>
2021-05-01 22:57:19 +0200

    refactor(server): pubsub to typescript

--- server/src/utils/pubsub.ts
+++ server/src/utils/pubsub.ts
@@ -5,9 +5,9 @@ const options = {
   retryStrategy: (times: number) => Math.min(times * 100, 3000),
 };
 
-const pubsub = new RedisPubSub({
+const pubsub: RedisPubSub = new RedisPubSub({
   publisher: new Redis(process.env.REDIS_URL, options),
   subscriber: new Redis(process.env.REDIS_URL, options),
 });
 
-module.exports = pubsub;
+export default pubsub;

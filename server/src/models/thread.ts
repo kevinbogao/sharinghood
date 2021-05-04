d70c9914e5ab7b43c9c18a0218ed4865093381ba
Kevin Gao <k_gao@aol.com>
2021-05-04 07:08:07 +0200

    fix(server): thread model community ref

--- server/src/models/thread.ts
+++ server/src/models/thread.ts
@@ -1,11 +1,13 @@
 import { Document, Schema, Types, model } from "mongoose";
 import { UserDocument } from "./user";
+import { CommunityDocument } from "./community";
 
 interface ThreadBaseDocument {
   content: string;
   poster: Types.ObjectId | UserDocument;
-  community: Types.ObjectId | UserDocument;
+  community: Types.ObjectId | CommunityDocument;
 }
+
 export interface ThreadDocument extends ThreadBaseDocument, Document {}
 
 const threadSchema: Schema = new Schema(

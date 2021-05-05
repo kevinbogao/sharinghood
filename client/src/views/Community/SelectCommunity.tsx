7e646b6f2f675fa82618f145349971dd6b15578b
Kevin Gao <k_gao@aol.com>
2021-05-05 23:57:02 +0200

    refactor(client): SelectCommunity to typescript

--- client/src/views/Community/SelectCommunity.tsx
+++ client/src/views/Community/SelectCommunity.tsx
@@ -1,7 +1,5 @@
-// @ts-nocheck
-
 import { useState } from "react";
-import PropTypes from "prop-types";
+import { Location, History } from "history";
 import {
   useQuery,
   useLazyQuery,
@@ -12,20 +10,34 @@ import Spinner from "../../components/Spinner";
 import InlineError from "../../components/InlineError";
 import ServerError from "../../components/ServerError";
 import { queries, mutations } from "../../utils/gql";
-import { validateForm } from "../../utils/helpers";
+import { typeDefs } from "../../utils/typeDefs";
+import { validateForm, FormError } from "../../utils/helpers";
 import { tokenPayloadVar, selCommunityIdVar } from "../../utils/cache";
 
-export default function SelectCommunity({ history, location }) {
-  let code;
+type State = { fromLogin?: boolean; communityCode?: string };
+
+interface SelectCommunityProps {
+  history: History;
+  location: Location<State>;
+}
+
+export default function SelectCommunity({
+  history,
+  location,
+}: SelectCommunityProps) {
+  let code: HTMLInputElement | null;
   const { fromLogin } = location.state || { fromLogin: false };
   const { communityCode } = location.state || { communityCode: null };
-  const [pageError, setPageError] = useState({});
-  const [isNewCommunity, setIsNewCommunity] = useState(false);
-  const [foundCommunity, setFoundCommunity] = useState(null);
   const tokenPayload = useReactiveVar(tokenPayloadVar);
   const selCommunityId = useReactiveVar(selCommunityIdVar);
+  const [pageError, setPageError] = useState<FormError>({});
+  const [isNewCommunity, setIsNewCommunity] = useState(false);
+  const [
+    foundCommunity,
+    setFoundCommunity,
+  ] = useState<typeDefs.Community | null>(null);
 
-  function selectCommunity(communityId) {
+  function selectCommunity(communityId: string) {
     // Store communityId in localStorage
     localStorage.setItem("@sharinghood:selCommunityId", communityId);
 
@@ -40,50 +52,56 @@ export default function SelectCommunity({ history, location }) {
 
   // Redirect user to posts page if selCommunityId exists (communityId)
   // in localStorage or user is only in one community.
-  const { loading, error, data } = useQuery(queries.GET_USER_COMMUNITIES, {
-    onCompleted: ({ communities }) => {
-      // Check if selectedCommunityId exists in communities array
-      const isIdInArray = communities.some(
-        (community) => community._id === selCommunityId
-      );
+  const { loading, error, data } = useQuery<typeDefs.UserCommunitiesData, void>(
+    queries.GET_USER_COMMUNITIES,
+    {
+      onCompleted: ({ communities }) => {
+        // Check if selectedCommunityId exists in communities array
+        const isIdInArray = communities.some(
+          (community) => community._id === selCommunityId
+        );
 
-      // If selected community id exists in localStorage & it the user is a
-      // member of that community
-      if (selCommunityId && isIdInArray) {
-        selectCommunity(selCommunityId);
+        // If selected community id exists in localStorage & it the user is a
+        // member of that community
+        if (selCommunityId && isIdInArray) {
+          selectCommunity(selCommunityId);
 
-        // Redirect to posts page
-        history.push("/find");
+          // Redirect to posts page
+          history.push("/find");
 
-        // If user is redirect from login and only has one community
-      } else if (communities.length === 1 && fromLogin) {
-        selectCommunity(communities[0]._id);
+          // If user is redirect from login and only has one community
+        } else if (communities.length === 1 && fromLogin) {
+          selectCommunity(communities[0]._id);
 
-        // Redirect to posts page
-        history.push("/find");
+          // Redirect to posts page
+          history.push("/find");
 
-        // If user is redirect from login and communityCode is given
-      } else if (fromLogin && communityCode) {
-        selectCommunity(communities[0]._id);
-      }
-      // eslint-disable-next-line
-    },
-    onError: ({ message }) => {
-      console.log(message);
-    },
-  });
+          // If user is redirect from login and communityCode is given
+        } else if (fromLogin && communityCode) {
+          selectCommunity(communities[0]._id);
+        }
+        // eslint-disable-next-line
+      },
+      onError: ({ message }) => {
+        console.warn(message);
+      },
+    }
+  );
 
   // Find community, limit user communities to 5
-  const [community] = useLazyQuery(queries.FIND_COMMUNITY_AND_MEMBERS, {
+  const [community] = useLazyQuery<
+    typeDefs.FindCommunityAndMembersData,
+    typeDefs.FindCommunityAndMembersVars
+  >(queries.FIND_COMMUNITY_AND_MEMBERS, {
     onCompleted: ({ community }) => {
-      if (community) {
+      if (community && tokenPayload) {
         // True if user is inside of community members array
         const userIsMember = community.members.some(
           (member) => member._id === tokenPayload.userId
         );
 
         // Throw error if user is in 5 communities already
-        if (data.communities.length >= 5)
+        if (data?.communities && data.communities.length >= 5)
           setPageError({
             code: "You have reached the maximum number of communities",
           });
@@ -113,13 +131,23 @@ export default function SelectCommunity({ history, location }) {
     {
       update(cache, { data: { joinCommunity } }) {
         // Get and update communities cache
-        const { communities } = cache.readQuery({
-          query: queries.GET_USER_COMMUNITIES,
-        });
-        cache.writeQuery({
-          query: queries.GET_USER_COMMUNITIES,
-          communities: [...communities, joinCommunity],
-        });
+        const userCommunitiesData = cache.readQuery<typeDefs.UserCommunitiesData>(
+          {
+            query: queries.GET_USER_COMMUNITIES,
+          }
+        );
+
+        if (userCommunitiesData) {
+          cache.writeQuery<typeDefs.UserCommunitiesData>({
+            query: queries.GET_USER_COMMUNITIES,
+            data: {
+              communities: [
+                ...userCommunitiesData.communities,
+                { ...joinCommunity, hasNotifications: false },
+              ],
+            },
+          });
+        }
 
         // Set community id
         selectCommunity(joinCommunity._id);
@@ -182,8 +210,9 @@ export default function SelectCommunity({ history, location }) {
                 type="button"
                 onClick={(e) => {
                   e.preventDefault();
-                  const errors = validateForm({ code }, setPageError);
-                  if (Object.keys(errors).length === 0) {
+                  const errors = validateForm({ code });
+                  setPageError(errors);
+                  if (Object.keys(errors).length === 0 && code) {
                     community({
                       variables: {
                         communityCode: code.value,
@@ -199,7 +228,7 @@ export default function SelectCommunity({ history, location }) {
                 className="main-btn block"
                 type="button"
                 onClick={() => {
-                  if (data.communities.length >= 5) {
+                  if (data?.communities && data.communities.length >= 5) {
                     setPageError({
                       code:
                         "You have reached the maximum number of communities",
@@ -249,7 +278,7 @@ export default function SelectCommunity({ history, location }) {
             className="main-btn block"
             type="button"
             onClick={() => {
-              if (data.communities.length >= 5) {
+              if (data?.communities && data.communities.length >= 5) {
                 setPageError({
                   code: "You have reached the maximum number of communities",
                 });
@@ -295,14 +324,3 @@ export default function SelectCommunity({ history, location }) {
     </div>
   );
 }
-
-SelectCommunity.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
-  location: PropTypes.shape({
-    state: PropTypes.shape({
-      fromLogin: PropTypes.bool,
-    }),
-  }).isRequired,
-};

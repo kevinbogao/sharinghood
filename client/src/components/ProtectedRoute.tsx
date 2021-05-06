2e12ec8a20c77779ee13d02eac697a1fbd47f179
Kevin Gao <k_gao@aol.com>
2021-05-06 04:09:09 +0200

    refactor(client): ProtectedRoute to typescript

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,11 +1,9 @@
-// @ts-nocheck
-
 import { Route, Redirect } from "react-router-dom";
 import { useReactiveVar } from "@apollo/client";
 import SelectCommunity from "../views/Community/SelectCommunity";
 import { accessTokenVar, selCommunityIdVar } from "../utils/cache";
 
-export default function ProtectedRoute({ component: Component, ...rest }) {
+export default function ProtectedRoute({ component: Component, ...rest }: any) {
   const accessToken = useReactiveVar(accessTokenVar);
   const selCommunityId = useReactiveVar(selCommunityIdVar);
 
@@ -18,6 +16,7 @@ export default function ProtectedRoute({ component: Component, ...rest }) {
             {selCommunityId ? (
               <Component {...props} communityId={selCommunityId} />
             ) : (
+              // @ts-ignore
               <SelectCommunity {...props} />
             )}
           </>
